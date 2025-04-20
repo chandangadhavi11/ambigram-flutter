17653f0132e36c7342ff6ad38dc976ccb0b8cf7b
chandan.gadhavi <chandan.gadhavi@mpokket.com>
2025-04-20 13:43:57 +0530

    Refactor editor quick suggestions and color palette options

--- lib/features/preview/presentation/screens/preview_screen.dart
+++ lib/features/preview/presentation/screens/preview_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:math';
-import 'dart:typed_data';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
